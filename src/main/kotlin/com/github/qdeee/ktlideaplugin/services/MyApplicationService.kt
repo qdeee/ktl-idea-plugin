60135b5fc1e22259630ce3a6e88048b196081993
GitHub Action <action@github.com>
2021-10-17 14:04:39 +0000

    Template cleanup

--- src/main/kotlin/com/github/qdeee/ktlideaplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/qdeee/ktlideaplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.qdeee.ktlideaplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.qdeee.ktlideaplugin.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/qdeee/ktlideaplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/qdeee/ktlideaplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.qdeee.ktlideaplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.qdeee.ktlideaplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
